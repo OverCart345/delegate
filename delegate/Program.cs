282d4ea4c8b79ab6dd6e9fd29eee4e0c07abf308
Ridiska34 <temich04om@mail.ru>
2023-04-15 23:52:13 +0600

    Добавьте файлы проекта.

---
+++ delegate/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace delegat
+{
+
+
+    delegate double Maths(double x, double y, double z);
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Maths summ = (x,y,z) => x + y + z;
+            Maths umn = (x, y, z) => x * y * z;
+            Maths sred = (x, y, z) => (x + y + z)/3;
+            Maths maxvalue = (x, y, z) => Math.Max(Math.Max(x, y), z);
+            Maths minvalue = (x, y, z) => Math.Min(Math.Min(x, y), z);
+
+            Console.WriteLine("Введите 3 значения");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Сумма - " + summ(a,b,c));
+            Console.WriteLine("Умножение - " + umn(a, b, c));
+            Console.WriteLine("среднее значение - " + sred(a, b, c));
+            Console.WriteLine("Максимальное значение - " + maxvalue(a, b, c));
+            Console.WriteLine("Минимальное значение - " + minvalue(a, b, c));
+
+            Console.ReadKey();
+        }
+    }
+}
